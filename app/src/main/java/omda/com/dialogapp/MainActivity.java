bb9faae58b5fdbc395fc3b339a5d0ff931abdcf5
briki <imedbriky@gmail.com>
2018-10-20 09:46:26 +0100

    Write a simple dialog app

---
+++ app/src/main/java/omda/com/dialogapp/MainActivity.java
@@ -0,0 +1,36 @@
+package omda.com.dialogapp;
+
+import android.content.DialogInterface;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Test a code from stackoverflow
+        final AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(this);
+        }
+        builder.setTitle("System Update")
+                .setMessage("A new version will be installed")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast toast = Toast.makeText(getApplicationContext(),"Upgrading", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                })
+                .show();
+    }
+}
